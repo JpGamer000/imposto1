52e987e8385ce56d6b3c59d99439399f7fec040d
JpGamer000 <163525423+JpGamer000@users.noreply.github.com>
2024-08-27 11:42:58 -0300

    Add files via upload

---
+++ imposto.js
@@ -0,0 +1,17 @@
+
+let salario = prompt ("digite seu salario");
+let  imposto1 = salario / 0.075;
+
+if (salario <= 2.259){
+    console.log("Paga nada de imposto");
+
+}else{
+
+        if (salario <= 2.826){
+            
+            console.log(`você pagara 7,5% de imposto que corresponde = ${imposto1}`)}
+
+        }
+
+
+    
